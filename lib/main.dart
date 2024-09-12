554e74cb3acb6308993ab04d4aba566842e92a7e
nitinkoberoii <lonewolfindia15@gmail.com>
2024-09-12 10:33:23 +0530

    updated file/folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:medi_track/screens/login/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-      ),
-      home: const Text('Flutter Demo Home Page'),
+      title: 'MediTrack',
+      theme: ThemeData(),
+      home: const LoginScreen(),
     );
   }
 }
